b1ec2582a1fccd116b133aed6b3cda259a35eac0
= <=>
2021-12-22 06:46:03 -0800

    Add file setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- This a Script thta prepares a Mysql
+-- Creation de of the BD hbnb_test_db, user and password
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
