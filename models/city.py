fcc15f673a4cd5f02328af5d00e1eef69a27aa89
= <=>
2021-12-22 13:40:54 -0800

    Update City.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+
+from os import getenv
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
 from sqlalchemy.orm import relationship
+from models import place
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
 
-    places = relationship("Place", backref="user", cascade="all, delete")
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('state_id'), nullable=False)
+        # place = relationship("Place", backref="cities",
+        # cascade="all, delete, delete-orphan")
+    else:
+        state_id = ""
+        name = ""
